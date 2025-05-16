4e3b7ce54d2328d4954bd7e88e946fb14f9078c7
Álvaro Becati <alvaro13hb@gmail.com>
2025-05-16 18:29:14 -0300

    reformulação do combate do personagem 1

--- character/character.gd
+++ character/character.gd
@@ -3,32 +3,56 @@ extends CharacterBody3D
 @onready var health_bar = $CanvasLayer/HealthBar
 @onready var audio_manager: Node2D = $"../AudioManager"
 @onready var camera: Camera3D = $CameraPivot/Camera/Camera3D
-@onready var cursor_camera: Camera3D = $CanvasLayer/SubViewportContainer/SubViewport/Camera3D2
-@onready var cursor: MeshInstance3D = $Cursor
 @onready var anim: AnimationPlayer = $Pivot/bartolomeu/AnimationPlayer
 
 @export var speed = 10
 @export var life = 10
 @export var jump_velocity = 25
 @export var max_jumps = 2
+@export var dodge_duration = 0.3
+@export var iframes_duration = 0.1
+@export var dodge_cooldown = 1.0
+@export var dodge_speed = 15.0
+
+
+var press_ticker = 0.0
 
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 var target_velocity = Vector3.ZERO
+var last_direction = Vector3.ZERO
 var dash_speed = speed * 5
 var dash_lenght = .1
 var is_attacking = false
-var cursor_pos
 var left_jumps = max_jumps
+
+var is_dodging = false
+var can_dodge = true
+var is_invincible = false
 
 
 func _ready() -> void:
-	cursor.basis.looking_at(camera.position)
 	health_bar.init_health(life)
 	Input.mouse_mode = Input.MOUSE_MODE_HIDDEN
 
 
+func start_dodge():
+	var dodge_direction = last_direction
+	dodge_direction.y = 0
+	
+	is_dodging = true
+	can_dodge = false
+	is_invincible = true
+	
+	await get_tree().create_timer(dodge_duration).timeout
+	is_dodging = false
+	await get_tree().create_timer(iframes_duration).timeout
+	is_invincible = false
+	
+	await get_tree().create_timer(dodge_cooldown).timeout
+	can_dodge = true
+
+
 func _physics_process(delta) -> void:
-	cursor_camera.global_transform = camera.global_transform
 	
 	if(!is_on_floor() and !is_attacking):
 		target_velocity.y -= gravity * delta
@@ -37,13 +61,11 @@ func _physics_process(delta) -> void:
 		left_jumps = max_jumps
 	
 	var direction = Vector3.ZERO
-	var last_direction = Vector3.ZERO
-	_look_at_cursor()
 	
 	if(!anim.is_playing()):
 		anim.play("idle")
 	
-	if(!is_attacking):
+	if(!is_attacking and !$Dash.is_dashing()):
 		if(Input.is_action_pressed("move_up")):
 			anim.play("walk")
 			direction.x += 1
@@ -60,44 +82,57 @@ func _physics_process(delta) -> void:
 			anim.play("walk")
 			direction.z -= 1
 			direction.x += 1
+			
+	if(Input.get_vector("move_left", "move_right", "move_up", "move_down") != Vector2(0, 0) and !$Dash.is_dashing()):
+		last_direction = direction
 	
-	if(Input.get_vector("move_left", "move_right", "move_up", "move_down") == Vector2(0, 0) and !is_attacking):
+	if(Input.get_vector("move_left", "move_right", "move_up", "move_down") == Vector2(0, 0) and !is_attacking and !$Dash.is_dashing()):
 		anim.play("idle")
 	
+	if(Input.is_action_pressed("attack") and !is_attacking):
+		press_ticker += 1
+		print(press_ticker)
+		if(press_ticker == 15.0):
+			if(is_on_floor()):
+				is_attacking = true
+				target_velocity.y = jump_velocity
+				anim.play("walk")
+				await anim.animation_finished
+				is_attacking = false
+			press_ticker = 0.0
 
-	if(Input.is_action_just_pressed("attack")):
-		$Pivot.look_at(cursor_pos, Vector3.UP)
-		is_attacking = true
-		anim.play("hit")
-		await anim.animation_finished
-		is_attacking = false
+	if(Input.is_action_just_released("attack") and !is_attacking):
+		if(press_ticker < 20): 
+			press_ticker = 0.0
+			is_attacking = true
+			if(Input.is_action_pressed("special") and is_on_floor()):
+				$Dash.start_dash(dash_lenght)
+				move_and_slide()
+			elif(!is_on_floor()):
+				target_velocity.y -= 1.35 * gravity
+			anim.play("hit")
+			await anim.animation_finished
+			is_attacking = false
+			
 	if(direction != Vector3.ZERO and !is_attacking):
 		direction = direction.normalized()
 		$Pivot.basis = Basis.looking_at(direction)
-	if(Input.is_action_just_pressed("dash")):
-		$Dash.start_dash(dash_lenght)
+	if(Input.is_action_just_pressed("dodge")):
+		start_dodge()
 	
 	var target_speed = dash_speed if $Dash.is_dashing() else speed
+	target_speed = dodge_speed if is_dodging else speed
+	if(direction == Vector3(0, 0, 0) and ($Dash.is_dashing() or is_dodging)):
+		direction = last_direction
 	
-	if(!is_attacking):
-		target_velocity.x = direction.x * target_speed
-		target_velocity.z = direction.z * target_speed
-		if(Input.is_action_just_pressed("jump") and left_jumps > 0):
-			left_jumps -= 1
-			target_velocity.y = jump_velocity
-		velocity = target_velocity
-		move_and_slide()
-
-
-func _look_at_cursor():
-	var player_pos = global_transform.origin
-	var drop_plane = Plane(Vector3(0, 1, 0), player_pos.y)
-	var ray_length = 1000
-	var mouse_pos = get_viewport().get_mouse_position()
-	var from = camera.project_ray_origin(mouse_pos)
-	var to = from + camera.project_ray_normal(mouse_pos) * ray_length
-	cursor_pos = drop_plane.intersects_ray(from, to)
-	cursor.global_transform.origin = cursor_pos + Vector3(0, 1, 0)
+	target_velocity.x = direction.x * target_speed
+	target_velocity.z = direction.z * target_speed
+	velocity = target_velocity
+		
+	if(!is_attacking and Input.is_action_just_pressed("jump") and left_jumps > 0):
+		left_jumps -= 1
+		target_velocity.y = jump_velocity
+	move_and_slide()
 
 
 func take_damage(amount: float) -> void:
